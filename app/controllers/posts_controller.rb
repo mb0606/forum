410baea1f1e621ca3f791c8c5ad0225c26ec4be2
Marco Berardini <mberardini@evolution-man.com>
2015-08-27 16:52:03 -0700

    Added authenication for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_filter :authenticate_user!, except: [:index, :show]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
